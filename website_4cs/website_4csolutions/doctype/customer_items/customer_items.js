3cb2c9c7f41915909d3cdc3626fe45a473daf397
Syed Mujeer Hashmi <mujeerhashmi@4csolutions.in>
2020-03-28 18:13:33 +0530

    customer carousel: Add customer carousel from doctype
    The customer carousel fetched images from customer doctype image but
    it was not proper to handle vehicles as customers to be shown on
    website. So we changed customer on website to be fetched similar to
    testimonial and intro carousel items.
    
    Signed-off-by: Syed Mujeer Hashmi <mujeerhashmi@4csolutions.in>

---
+++ website_4cs/website_4csolutions/doctype/customer_items/customer_items.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, 4C Solutions and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Customer Items', {
+	// refresh: function(frm) {
+
+	// }
+});
