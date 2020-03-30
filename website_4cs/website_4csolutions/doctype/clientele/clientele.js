816bc69e232018e6e6cfcebfa6948f5d8b947a74
Syed Mujeer Hashmi <mujeerhashmi@4csolutions.in>
2020-03-30 06:30:34 +0530

    clientele carousel: Add clientele carousel from doctype
    The clientele carousel fetched images from customer doctype image but
    it was not proper to handle vehicles as customers to be shown on
    website. So we changed customer on website to be fetched similar to
    testimonial and intro carousel items.
    
    Signed-off-by: Syed Mujeer Hashmi <mujeerhashmi@4csolutions.in>

---
+++ website_4cs/website_4csolutions/doctype/clientele/clientele.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, 4C Solutions and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Clientele', {
+	// refresh: function(frm) {
+
+	// }
+});
