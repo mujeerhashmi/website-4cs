a533d94ca27417e438a5ef6b0f7ec856f63f07cc
Syed Mujeer Hashmi <mujeerhashmi@4csolutions.in>
2020-06-15 07:48:04 +0530

    Intro: Removed carousel & added text-slider with particles
    Signed-off-by: Syed Mujeer Hashmi <mujeerhashmi@4csolutions.in>

--- website_4cs/public/js/main.js
+++ website_4cs/public/js/main.js
@@ -160,6 +160,18 @@
     allowPageScroll:"vertical"
   });
 
+  /*--/ Star Typed /--*/
+	if ($('.text-slider').length == 1) {
+    var typed_strings = $('.text-slider-items').text();
+		var typed = new Typed('.text-slider', {
+			strings: typed_strings.split(','),
+			typeSpeed: 80,
+			loop: true,
+			backDelay: 1100,
+			backSpeed: 30
+		});
+	}
+  
   // Skills section
   $('#skills').waypoint(function() {
     $('.progress .progress-bar').each(function() {
